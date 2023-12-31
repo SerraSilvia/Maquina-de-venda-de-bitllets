faf2080db4f4a7b264e5ac6ac0e41cd50f77758d
Silvia <silvia.serra.7e6@itb.cat>
2023-12-31 13:37:37 +0100

    Add ascci art + opcions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import org.example.BLUE_BOLD
+import org.example.CYAN_BOLD
 import org.example.RESET
 
 /**
@@ -12,7 +13,7 @@ import org.example.RESET
  * @return Benvinguda al programa
  */
 fun printAsciiArt(){
-    val asciiArt= """   $BLUE_BOLD
+    val asciiArt= """   $CYAN_BOLD
         *************************************************
         *  BENVINGUTS A LA XARXA DE TRANSPORTS DE BCN   *
         * _____ __  __ ____            _____ ____  ____ *
@@ -40,18 +41,68 @@ fun opcioBitllet(opcio:Int):Int{
             4-> println("TFamiliar")
             5-> println("TJove")
             6-> println("Sortir")
+        else-> println("Escriu un número vàlid")
     }
     return opcio
 }
 
+fun preusPerZona1(opcio:Int):Double{
+   return when (opcio){
+        1-> 2.40
+        2-> 11.35
+        3-> 40.00
+        4-> 10.00
+        5-> 80.00
+        else-> 0.0
+    }
+}
+fun preusPerZona2(opcio:Int):Double{
+    return when (opcio){
+        1-> 2.40*1.3125
+        2-> 11.35*1.3125
+        3-> 40.00*1.3125
+        4-> 10.00*1.3125
+        5-> 80.00*1.3125
+        else-> 0.0
+    }
+}
+
+fun preusPerZona3(opcio:Int):Double{
+    return when (opcio){
+        1-> 2.40*1.8443
+        2-> 11.35*1.8443
+        3-> 40.00*1.8443
+        4-> 10.00*1.8443
+        5-> 80.00*1.8443
+        else-> 0.0
+    }
+}
+
 fun main() {
         printAsciiArt()
-        println("$BLUE_BOLD \n Escull el número de l' opció desitjada: $RESET")
+        println("$BLUE_BOLD \n Quin bitllet desitja adquirir? $RESET")
     val paraulaSeguretat = 4321
+    val seguirComprant= "n".lowercase()
 
     do {
         val opcio= readln().toInt()
         println(opcioBitllet(opcio))
 
-    }while (opcio != 6 && opcio != paraulaSeguretat)
+        println("$BLUE_BOLD Quina zona vol viatjar?  $RESET")
+        val zona= readln().toInt()
+        if (zona==1){
+            preusPerZona1(opcio)
+        }else if (zona == 2){
+            preusPerZona2(opcio)
+        }else if (zona==3){
+            preusPerZona3(opcio)
+        }
+
+        println("Ha escollit la opció: $opcio, zona $zona ")
+        println("El preu del bitllet és de: "+preusPerZona1(opcio))
+        println("Vols seguir comprant? [S,N]")
+
+
+    }while (opcio != 6 && opcio != paraulaSeguretat && opcio != seguirComprant)
+    println("Fi de programa")
 }
