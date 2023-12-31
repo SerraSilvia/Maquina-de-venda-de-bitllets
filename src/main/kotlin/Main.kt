cd4cb23b8958fd8c9ab2df398cd54cc9ea73f6d3
Silvia <silvia.serra.7e6@itb.cat>
2023-12-31 14:37:26 +0100

    Update main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import org.example.BLUE_BOLD
 import org.example.CYAN_BOLD
 import org.example.RESET
+import java.util.*
 
 /**
  * És una funció de benvinguda al programa amb un ascii art
@@ -33,17 +34,26 @@ fun printAsciiArt(){
  * @param opcio L'usuari escull la opció desitjada
  * @return Opció triada
  */
-fun opcioBitllet(opcio:Int):Int{
-    when (opcio){
-            1-> println("Bitllet senzill")
-            2-> println("TCasual")
-            3-> println("TUsual")
-            4-> println("TFamiliar")
-            5-> println("TJove")
-            6-> println("Sortir")
-        else-> println("Escriu un número vàlid")
-    }
-    return opcio
+fun opcioBitllet():Int{
+    println("1.- Bitllet senzill")
+    println("2.- TCasual")
+    println("3.- TUsual")
+    println("4.- TFamiliar")
+    println("5.- TJove")
+    println("6.- Sortir")
+    println("$BLUE_BOLD \n Quin bitllet desitja adquirir? $RESET")
+
+    var opcio: Int
+
+    do {
+        opcio = readln().toInt()
+
+        if (opcio > 6 || opcio < 1)
+            println("Escriu un número vàlid")
+
+    } while(opcio > 6 || opcio < 1)
+
+   return opcio
 }
 
 fun preusPerZona1(opcio:Int):Double{
@@ -80,29 +90,32 @@ fun preusPerZona3(opcio:Int):Double{
 
 fun main() {
         printAsciiArt()
-        println("$BLUE_BOLD \n Quin bitllet desitja adquirir? $RESET")
     val paraulaSeguretat = 4321
-    val seguirComprant= "n".lowercase()
+    var seguirComprant = "n".lowercase()
+    var exist = false;
 
     do {
-        val opcio= readln().toInt()
-        println(opcioBitllet(opcio))
+        val opcio = opcioBitllet()
 
-        println("$BLUE_BOLD Quina zona vol viatjar?  $RESET")
-        val zona= readln().toInt()
-        if (zona==1){
-            preusPerZona1(opcio)
-        }else if (zona == 2){
-            preusPerZona2(opcio)
-        }else if (zona==3){
-            preusPerZona3(opcio)
+        if (opcio != 6) {
+            println("$BLUE_BOLD Quina zona vol viatjar?  $RESET")
+            val zona= readln().toInt()
+            if (zona==1){
+                preusPerZona1(opcio)
+            }else if (zona == 2){
+                preusPerZona2(opcio)
+            }else if (zona==3){
+                preusPerZona3(opcio)
+            }
+
+            println("Ha escollit la opció: $opcio, zona $zona ")
+            println("El preu del bitllet és de: "+preusPerZona1(opcio))
+            println("Vols seguir comprant? [S,N]")
+            seguirComprant = readln().toString().lowercase(Locale.getDefault())
         }
 
-        println("Ha escollit la opció: $opcio, zona $zona ")
-        println("El preu del bitllet és de: "+preusPerZona1(opcio))
-        println("Vols seguir comprant? [S,N]")
+        if (seguirComprant == "n") exist = true
 
-
-    }while (opcio != 6 && opcio != paraulaSeguretat && opcio != seguirComprant)
+    }while (opcio != 6 && opcio != paraulaSeguretat && !exist)
     println("Fi de programa")
 }
