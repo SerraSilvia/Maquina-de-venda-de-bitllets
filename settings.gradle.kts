cf2ec07d9a2635ad59f954db3a2e80d86d057a06
Silvia <silvia.serra.7e6@itb.cat>
2023-12-31 11:32:23 +0100

    creació de projecte

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "maquina-de-venda-de-bitllets"
